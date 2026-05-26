2608b8d89f88bfe0d29120936f6141bb00e285a2
João Batista <cardosofiles@outlook.com>
2026-05-26 13:41:04 -0300

    feat(use-cases): Implementando caso de uso de busca de usuário
    feat(use-cases): Implementando caso de uso de busca de usuário
    
    Co-Authored-By: João Batista <contatojoaomiranda@outlook.com>

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,9 +1,13 @@
-import type { Prisma } from '@/generated/prisma/client.js';
+import type { Prisma, User } from '@/generated/prisma/client.js';
 import type { UsersRepository } from '@/repositories/users-repository.js';
 
 import { prisma } from '@/db/prisma.js';
 
 export class PrismaUsersRepository implements UsersRepository {
+  findById(id: string): Promise<User | null> {
+    throw new Error('Método não implementado.');
+  }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
