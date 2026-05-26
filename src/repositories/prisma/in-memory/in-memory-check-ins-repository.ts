10e6e67dfe4cf7df8a12e1e1de71580d5ba63491
João Batista <cardosofiles@outlook.com>
2026-05-26 15:11:15 -0300

    feat(use-cases): Trabalhando com TDD e Mocking
    Co-Authored-By: João Batista <contatojoaomiranda@outlook.com>

--- src/repositories/prisma/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/prisma/in-memory/in-memory-check-ins-repository.ts
@@ -6,6 +6,20 @@ import type { CheckInsRepository } from '@/repositories/check-ins-repository.js'
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = [];
 
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const checkInOnSameDate = this.items.find((checkIn) => {
+      const isSameUser = checkIn.user_id === userId;
+      const isSameDate =
+        checkIn.created_at.getUTCFullYear() === date.getUTCFullYear() &&
+        checkIn.created_at.getUTCMonth() === date.getUTCMonth() &&
+        checkIn.created_at.getUTCDate() === date.getUTCDate();
+
+      return isSameUser && isSameDate;
+    });
+
+    return checkInOnSameDate || null;
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
       id: randomUUID(),
