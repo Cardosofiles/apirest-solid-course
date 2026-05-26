b764c3958a380e9fc34e8a6dcb6d8c11fe643ff8
João Batista <cardosofiles@outlook.com>
2026-05-26 09:17:45 -0300

    feat(unit-test-create-user): Implementando três testes de criação de usuário
    Co-Authored-By: João Batista <contatojoaomiranda@outlook.com>

---
+++ src/repositories/prisma/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,23 @@
+import type { Prisma, User } from '@/generated/prisma/client.js';
+import type { UsersRepository } from '@/repositories/users-repository.js';
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public items: User[] = [];
+
+  async findByEmail(email: string) {
+    const user = this.items.find((item) => item.email === email);
+    return user || null;
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: 'user-id',
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    };
+    this.items.push(user);
+    return user;
+  }
+}
