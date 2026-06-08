bd49bb2a940140f7cd918c4c20244a917ab6396e
João Batista <cardosofiles@outlook.com>
2026-06-08 11:42:13 -0300

    feat(controller): implementando controller de criar e listar academia
    feat(controller): implementando controller de criar e listar academia
    
    Co-Authored-By: João Batista <contatojoaomiranda@outlook.com>

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,7 +1,15 @@
 import type { FastifyInstance } from 'fastify';
 
+import { create } from '@/http/controllers/gyms/create.js';
+import { nearby } from '@/http/controllers/gyms/nearby.js';
+import { search } from '@/http/controllers/gyms/search.js';
 import { verifyJwt } from '@/http/middlewares/verify-jwt.js';
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt);
+
+  app.get('/gyms/search', search);
+  app.get('/gyms/nearby', nearby);
+
+  app.post('/gyms', create);
 }
---
+++ src/http/controllers/gyms/schemas/search-query-schema.ts
@@ -0,0 +1,6 @@
+import { z } from 'zod';
+
+export const searchGymsQuerySchema = z.object({
+  queryParams: z.string(),
+  page: z.coerce.number().min(1).default(1),
+});
